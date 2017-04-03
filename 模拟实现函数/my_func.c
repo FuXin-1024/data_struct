3db399e57c4912c9e9b9ee58826e8d82c4fc2ed1
FuXin-1024 <1007160080@qq.com>
2017-04-03 15:23:27 +0800

    增加memmove,memcpy

--- 模拟实现函数/my_func.c
+++ 模拟实现函数/my_func.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<assert.h>
 
 char* my_strcpy(char* dst, const char* src)
@@ -14,7 +15,7 @@ char* my_strcpy(char* dst, const char* src)
 	return ret;
 }
 
-char* my_strncpy(char* dst, const char* src,int n)
+char* my_strncpy(char* dst, const char* src, int n)
 {
 	assert(dst);
 	assert(src);
@@ -43,7 +44,7 @@ char* my_strcat(char* dst, const char* src)
 	{
 		*dst++ = *src++;
 	}
-	
+
 	return ret;
 }
 
@@ -56,11 +57,11 @@ char* my_strncat(char* dst, const char* src, int n)
 	{
 		dst++;
 	}
-	
+
 	while (n--)
 	{
 		*dst++ = *src++;
-		
+
 	}
 	return ret;
 }
@@ -87,7 +88,7 @@ int my_strcmp(const char* str1, const char* str2)
 		return -1;
 }
 
-int my_strncmp(const char* str1, const char* str2,int n)
+int my_strncmp(const char* str1, const char* str2, int n)
 {
 	assert(str1);
 	assert(str2);
@@ -106,15 +107,75 @@ int my_strncmp(const char* str1, const char* str2,int n)
 	}
 	return 0;
 }
+void* my_memmove(void* dest, const void* src, size_t n)
+{
+	assert(dest);
+	assert(src);
+	char* pdest = (char*)dest;
+	const char*psrc = ( const char*)src;
+	
+	//正常情况下，从前往后拷贝
+	if (pdest <= psrc&& pdest >= psrc + n)
+	{
+		while (n--)
+		{
+			*pdest++ = *psrc;
+		}
+	}
+	//内存有重叠时，从后往前拷贝
+	else
+	{
+		while (n--)
+		{
+			*(pdest +n) = *(psrc+n);
+		}
+	}
+	return dest;
+
+}
+
+void* my_memcpy(void* dest, const void* src, size_t n)
+{
+	assert(dest);
+	assert(src);
+	char* pdest = (char*)dest;
+	const char* psrc = (const char*)src;
+	while (n--)
+	{
+		*pdest++ = *psrc++;
+	}
+	return dest;
+}
+void Test_Memmove()
+{
+	char arr[10] = "abcdefg";
+	char arr0[10] = "abcdefg";
+	char arr1[10] = { 0 };
+	my_memmove(arr + 2, arr, 4);
+	my_memmove(arr1, arr0, 4);
+	printf("内存覆盖情况：%s\n", arr + 2);
+	printf("正常情况：%s\n", arr1);
+}
+void Test_Memcpy()
+{
+	char arr[10] = "abcdefg";
+	char arr0[10] = "abcdefg";
+	char arr1[10] = { 0 };
+	my_memcpy(arr + 2, arr, 4);
+	my_memcpy(arr1, arr0, 4);
+	printf("内存覆盖情况：%s\n", arr+2);
+	printf("正常情况：%s\n", arr1);
+}
 void Test()
 {
 	char s[30] = "abkd";
 	char s1[] = "abgh";
-	printf("%d\n", my_strncmp(s, s1,6));
+	printf("%d\n", my_strncmp(s, s1, 6));
 }
 int  main()
 {
-	Test();
+	//Test();
+	Test_Memcpy();
 	system("pause");
 	return 0;
 }
