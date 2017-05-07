5253bc622454148daee423a44ed19fffc2d6d8a2
FuXin-1024 <1007160080@qq.com>
2017-05-07 19:48:05 +0800

    B树

--- BinaryTree/BTree.h
+++ BinaryTree/BTree.h
@@ -2,13 +2,13 @@
 #include<iostream>
 using namespace std;
 
-template <class K,class M=3>
+template <class K,size_t M=3>
 struct BTreeNode
 {
-	K _key[M];//多一个空间，方便分裂
-	BTreeNode<K, M>* _subs[M + 1];//多分配一个空间
-	BTreeNode<K, M>* _parent;//父节点
-	size_t _size;
+	K _keys[M];						//多一个空间，方便分裂
+	BTreeNode<K, M>* _subs[M + 1];	//多分配一个空间
+	BTreeNode<K, M>* _parent;		//父节点
+	size_t _size;					//实际key的个数
 
 	BTreeNode()
 	{
@@ -18,16 +18,59 @@ struct BTreeNode
 	}
 };
 
-template <class K,class M=3>
+template <class K, size_t M = 3>
 class BTree
 {
-	typedef BTreeNode Node;
+	typedef BTreeNode<K,M> Node;
 public:
 	BTree()
 		:_root(NULL)
 	{}
 
+	pair <Node* ,int>  Find(const K& key)
+	{
+		Node* parent = cur;
+		Node* cur = _root;
+		while (cur)
+		{
+			size_t i = 0;
+			for (; i < cur->_size;)
+			{
+				if (cur->_keys[i] < key)
+					++i;
+				else if (cur->_keys[i] > key)
+					break;
+				else
+					return make_pair(cur, i);
+			}
+			parent = cur;
+			cur = cur->_keys[i];
+		}
+		return make_pair(parent, -1);
+	}
+
 	bool Insert(const K& key)
+	{
+		if (root == NULL)
+		{
+			_root = new node;
+			_root->_keys[0] = key;
+			_root->_size = 1;
+			return true;
+		}
+		pair<Node*, int> ret = Find(key);
+		if (ret.second >= 0)//key已经存在
+			return false;
+		Node* cur = ret.first;
+		Node* sub = cur;
+		K newKey = key;
+		while (1)
+		{
+			_Insert(cur, sub, key);
+		}
+	}
+protected:
+	void _Insert(Node* cur, Node* sub, const K& key)
 	{
 
 	}
