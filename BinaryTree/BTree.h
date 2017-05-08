1b92a067b0d4886d7dfdd5bd2af58d373c9038de
FuXin-1024 <1007160080@qq.com>
2017-05-08 15:12:06 +0800

    B树

--- BinaryTree/BTree.h
+++ BinaryTree/BTree.h
@@ -30,7 +30,7 @@ public:
 
 	pair <Node* ,int>  Find(const K& key)
 	{
-		Node* parent = cur;
+		Node* parent = NULL;
 		Node* cur = _root;
 		while (cur)
 		{
@@ -45,16 +45,16 @@ public:
 					return make_pair(cur, i);
 			}
 			parent = cur;
-			cur = cur->_keys[i];
+			cur = cur->_subs[i];
 		}
 		return make_pair(parent, -1);
 	}
 
 	bool Insert(const K& key)
 	{
-		if (root == NULL)
+		if (_root == NULL)
 		{
-			_root = new node;
+			_root = new Node;
 			_root->_keys[0] = key;
 			_root->_size = 1;
 			return true;
@@ -71,7 +71,7 @@ public:
 			_Insert(cur, sub, newKey);
 
 			//判断关键字数量是否满足
-			if (cur->_size < M) //关键字数量最大为M-1
+ 			if (cur->_size < M) //关键字数量最大为M-1
 				return true;
 			//不满足，开始分裂
 			Node* newNode = new Node;
@@ -86,9 +86,10 @@ public:
 				{
 					cur->_subs[i]->_parent = newNode;
 				}
+				++index;
 				newNode->_size++;
 			}
-			newNode->_keys[i] = cur->_keys[i];//最后一个元素
+			newNode->_subs[i] = cur->_subs[i];//最后一个元素
 			if (cur->_subs[i])
 			{
 				cur->_subs[i]->_parent = newNode;
@@ -109,10 +110,16 @@ public:
 			else
 			{
 				newKey = cur->_keys[mid];
-				cur = parent;
+				cur = cur->_parent;
 				sub = newNode;
 			}
 		}
+	}
+
+	void InOrder()
+	{
+		_InOrder(_root);
+		cout << endl;
 	}
 protected:
 	void _Insert(Node* cur, Node* sub, const K& key)
@@ -122,22 +129,46 @@ protected:
 		for (; end >= 0; --end)
 		{
 			//key按升序排列
-			if (cur->_keys[end] > key)
+			if (cur->_keys[end] < key)
 				break;
 			else
 			{
 				//把key插入到合适位置、
 				cur->_keys[end+1] = cur->_keys[end];
-				sub->_keys[end + 2] = sub->_keys[end + 1];//把右孩子向后移
+				cur->_subs[end + 2] = cur->_subs[end + 1];//把右孩子向后移
 			}
-
+		}
 			cur->_keys[end + 1] = key;
 			cur->_subs[end + 2] = sub;
 			if (sub)
 				sub->_parent = cur;
-			cur->_size + 1;
+			cur->_size++;
+	}
+
+	void _InOrder(Node* root)
+	{
+		if (root == NULL)
+			return;
+		size_t i = 0;
+		for (; i < root->_size; ++i)
+		{
+			_InOrder(root->_subs[i]);
+			cout << root->_keys[i]<<" ";
 		}
+		_InOrder(root->_subs[i]);
 	}
 protected:
 	Node* _root;
 };
+
+void BTreeTest()
+{
+	int arr[] = { 53, 75, 139, 49, 145, 36, 101 };
+	BTree<int, 3> t;
+	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
+	{
+		t.Insert(arr[i]);
+	}
+	cout << endl;
+	t.InOrder();
+}
