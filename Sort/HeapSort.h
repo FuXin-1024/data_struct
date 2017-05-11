d11b10d9ba29f35c1dc35a662bd538d01e36c231
FuXin-1024 <1007160080@qq.com>
2017-05-11 21:40:59 +0800

    堆排序

--- Sort/HeapSort.h
+++ Sort/HeapSort.h
@@ -2,37 +2,56 @@
 #include<assert.h>
 using namespace std;
 
-void AdjustUp(int* a, int i, int n)//从第一个非叶子结点开始调整
+//升序，建大堆
+void AdjustDown(int* heap, size_t n, size_t parent)
 {
-	assert(a);
-	int j = (i - 1) / 2;
-	int tmp = a[i];
-	
-	while (j >= 0 && i != 0)
+	size_t child = parent * 2 + 1;
+	while (child < n)
 	{
-		if (a[j] <= tmp)
+		if (child + 1 < n&&heap[child + 1] > heap[child])
+		{
+			++child;
+		}
+
+		if (heap[child] > heap[parent])
+		{
+			swap(heap[child], heap[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
 			break;
-		a[i] = a[j];
-		i = j;
-		j = (i - 1) / 2;
+		}
 	}
-	a[i] = tmp;
 }
 
-void MakeMinHeap(int* a,int n)
+void HeapSort(int* a, int n)
 {
 	assert(a);
-	for(int i = n / 2 - 1; i >= 0; --i)
+	int parent = (n - 2) >> 1;
+	for (; parent >= 0; --parent)
 	{
-		AdjustUp(a, i, n);
+		AdjustDown(a, n, parent);
+	}
+
+	//堆顶与最后一个数交换，除最后一个数，调整成大堆。循环。保证每个堆的最后一个数是最大的
+	for (int i = n - 1; i >= 0; --i)
+	{
+		swap(a[0], a[i]);
+		AdjustDown(a, i, 0);
 	}
 }
 
-void HeapSort(int* a, int len)
+void HeapSortTest()
 {
-	for (int i = len - 1; i >= 1; --i)
+	int arr[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
+
+	HeapSort(arr, sizeof(arr) / sizeof(arr[0]));
+	cout << "HeaptSort: ";
+	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 	{
-		swap(a[i], a[0]);
-		AdjustUp(a, 0, i);
+		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
