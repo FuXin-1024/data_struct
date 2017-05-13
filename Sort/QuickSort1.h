fee098e4f2e8742afda59a9901f47f9e4129c0ac
FuXin-1024 <1007160080@qq.com>
2017-05-13 21:51:20 +0800

    左右指针交换法，快速排序

--- Sort/QuickSort1.h
+++ Sort/QuickSort1.h
@@ -30,6 +30,27 @@ int PartSort(int* a, int left, int right)
 {
 	assert(a);
 	int mid = GetMidindex(a, left, right);//三数取中
+	swap(a[mid], a[right]);
+	int key = a[right];
+	int begin = left;
+	int end = right;
+
+	while (begin < end)
+	{
+		while (begin < end && a[begin] <= key)//找大
+		{
+			++begin;
+		}
+
+		while (begin<end && a[end]>key)//找小
+		{
+			--end;
+		}
+		if (begin < end)
+			swap(a[begin], a[end]);
+	}
+	swap(a[begin], a[right]);
+	return begin;
 }
 
 void QuickSort(int* a, int left,int right)
@@ -41,4 +62,16 @@ void QuickSort(int* a, int left,int right)
 
 	QuickSort(a, left, div - 1);
 	QuickSort(a, div + 1,right);
+}
+
+void QuickSorttTest()
+{
+	int arr[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
+	QuickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	cout << "QuickSort: ";
+	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 }
