fb1ed6be95228a44923f50299b44babca174e232
FuXin-1024 <1007160080@qq.com>
2017-05-13 20:34:13 +0800

    冒泡排序

--- Sort/BubbleSort.h
+++ Sort/BubbleSort.h
@@ -1,7 +1,9 @@
 #pragma once
 #include<iostream>
+#include<assert.h>
 using namespace std;
 
+//ц╟ещеепР
 void SubbleSort(int* arr, int len)
 {
 	for (int i = 0; i < len; ++i)
@@ -30,10 +32,23 @@ void SubbleSort1(int* arr, int len)
 		n--;
 	}
 }
+
+void SubbleSort2(int* a, int n)
+{
+	assert(a);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 1; j < n-i; ++j)
+		{
+			if (a[j-1]>a[j])
+				swap(a[j-1], a[j]);
+		}
+	}
+}
 void SubbleSortTest()
 {
 	int arr[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
-	SubbleSort1(arr, sizeof(arr) / sizeof(arr[0]));
+	SubbleSort2(arr, sizeof(arr) / sizeof(arr[0]));
 	cout << "SubbleSort: ";
 	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 	{
