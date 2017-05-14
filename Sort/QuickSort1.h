71ecde2b10db033b6f2971f768f58bdc80da9f40
FuXin-1024 <1007160080@qq.com>
2017-05-14 21:13:45 +0800

    增加非递归实现快速排序

--- Sort/QuickSort1.h
+++ Sort/QuickSort1.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<iostream>
+#include<stack>
 #include<assert.h>
 using namespace std;
 
@@ -102,6 +103,15 @@ void QuickSort(int* a, int left,int right)
 	assert(a);
 	if (left >= right)
 		return;
+
+	//小区间优化
+	/*if (right - left < 4)
+	{
+		InsertSort(a, right - left);
+	}*/
+	/*else
+	{
+	}*/
 	//int div = PartSort2(a, left, right);
 	int div = PartSort3(a, left, right);
 
@@ -109,10 +119,37 @@ void QuickSort(int* a, int left,int right)
 	QuickSort(a, div + 1,right);
 }
 
+//非递归实现快速排序
+void QuickSortNoR(int* a, int left, int right)
+{
+	stack <int> s;
+	s.push(right);
+	s.push(left);
+
+	while (!s.empty())
+	{
+		int begin = s.top();
+		s.pop();
+		int end = s.top();
+		s.pop();
+		
+		int div = PartSort3(a,begin,end);
+		if (div - 1 > begin)
+		{
+			s.push(div - 1);
+			s.push(begin);
+		}
+		if (div + 1 < end)
+		{
+			s.push(end);
+			s.push(div + 1);
+		}
+	}
+}
 void QuickSorttTest()
 {
 	int arr[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
-	QuickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	QuickSortNoR(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
 	cout << "QuickSort: ";
 	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 	{
