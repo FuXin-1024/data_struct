9ac2135e61b9967b1f0e16f6c9bceb3180ee9cbd
FuXin-1024 <1007160080@qq.com>
2017-05-19 16:59:42 +0800

    计数排序

---
+++ Sort/CountSort.h
@@ -0,0 +1,47 @@
+#pragma once 
+#include<iostream>
+#include<assert.h>
+#include<memory.h>
+#include<string.h>
+using namespace std;
+void CountSort(int* a, int n)
+{
+	assert(a);
+	int min = a[0];
+	int max = a[0];
+
+	for (int i = 1; i < n; ++i)
+	{
+		if (a[i] > max)
+			max = a[i];
+		if (a[i] < min)
+			min = a[i];
+	}
+	int range = max - min + 1;
+	int* counts = new int(range);
+	memset(counts, 0, sizeof(range));
+
+	for (int i = 0; i < n; ++i)
+	{
+		counts[a[i] - min]++;
+	}
+
+	int j = 0;
+	for (int i = 0; i < range; ++i)
+	{
+		while (counts[i]--)
+			a[j++] = i + min;
+	}
+}
+
+void CountSortTest()
+{
+	int arr[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
+	CountSort(arr, sizeof(arr) / sizeof(arr[0]));
+	cout << "CountSort: ";
+	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
