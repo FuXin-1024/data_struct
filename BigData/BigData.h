60b93ca252e6564aff07ff1a4728d94ef48415cf
FuXin-1024 <1007160080@qq.com>
2017-07-26 15:58:17 +0800

    大数运算，加减乘

--- BigData/BigData.h
+++ BigData/BigData.h
@@ -1,57 +1,99 @@
 #pragma once
+#define _CRT_SECURE_NO_DEPRECATE 1
 #include<iostream>
 #include<string>
 using namespace std;
 
+typedef long long INT64;    //long long 8字节
+const INT64 UN_INIT = 0xcccccccccccccccc;
 const long long MAX = 0x7fffffffffffffff;
 const long long MIN = 0x8000000000000000;
+#define MAXLEN  18
 
 class BigData
 {
 public:
-	BigData(long long value)
-	:_value(value)
+
+	BigData()
+		:_value(UN_INIT)
 	{}
 
+	BigData(INT64 num)
+		:_value(num)
+	{
+		char val[30];
+		sprintf(val, "%d", num);
+		_strData = string(val);
+	}
 	BigData(const string& strData)
 		:_value(0)
 		, _strData("+0")
 	{
-		//空字符串
-		if (strData.empty())
+		//去空串
+		if (strData.empty())  //空串不做处理
+		{
+			return;
+		}
+		//去前面的空格；"   02316"          2316
+		char *pData = (char*)strData.c_str();
+		while (isspace(*pData))
+		{
+			pData++;
+		}
+		//"       "   处理完后，变为空串
+		if (*pData == '\n')
 			return;
 
-		//             123456789
-		char* data = (char*)strData.c_str();
-		while (isspace(*data))
-			++data;
-		if (*data == '\0')
-			return;
-		// +12345678
-		char symbol = *data; //先把符号位保存下来
-		if (isdigit(*data))
-			symbol = '+';
-		else if (*data == '+' || *data == '-')
-			++data;
-		else
-			return;
-		
-		//000000000000123456789
-		while ('0' == *data)
-			++data;
-		//直接转换
-		_strData.resize(strlen(data) + 1);
-		_strData[0] = symbol;
-		size_t count = 1;
-		while (isdigit(*data))
+		//处理符号
+		char symbol;
+		if (isdigit(*pData))
 		{
-			_value = _value * 10 + (*data - '0');
-			_strData[count++] = *data;
-			++ data;
+			symbol = '+';
 		}
-		if ('-' == symbol)
+		else if (*pData == '+' || *pData == '-')
+		{
+			symbol = *pData;
+			pData++;
+		}
+		else
+		{
+			return;   //处理"abc123465"         0
+		}
+
+		//去掉前面的零
+		while (*pData == '0')
+		{
+			pData++;
+		}
+
+		//全零处理
+		if (*pData == '\0')
+			return;
+
+		//至此，所有字符串都转为正确的格式 "12365"  "ac23"  "1562aa" "135+362" "ac0000"-->"0" 均只处理为数字格式->"1256"
+		int count = 1;
+		_strData.resize(strlen(pData) + 1);
+		_strData[0] = symbol;
+		while (isdigit(*pData))
+		{
+			_value = _value * 10 + (*pData - '0');
+			_strData[count++] = *pData;
+			pData++;
+		}
+		//处理只剩下符号的情况的情况
+		if (_strData == "+" || strData == "-")
+		{
+			_strData = "+0";
+			return;
+		}
+
+		if (symbol == '-')
 			_value = 0 - _value;
-		_strData.resize(count);		
+
+
+		//处理提取有效字符串部分；
+		if (*pData == '\0')    //处理"000+123456"   到这一步剩下_strData = "+0" 而count为1，resize之后_strData变为"+";
+			_strData.resize(count);
 	}
 	string GetStrData()
 	{
@@ -107,6 +149,71 @@ public:
 				return BigData(StrAdd(_strData, b._strData));
 			}
 		}
+		else//没有超出范围，但是结果可能溢出
+		{
+			//同号相减一定不会溢出
+			if (_strData[0] == b._strData[0])
+				return BigData(_value - b._value);
+			else
+			{
+				//MAX - | 左操作数 | >|右操作数|
+				if (MAX - abs(_value) > abs(b._value))
+					return BigData(_value - b._value);
+				else
+					return BigData(StrAdd(_strData, b._strData));
+			}
+		}
+	}
+
+	BigData operator*(const BigData& b)
+	{
+		return BigData(StrMul(_strData, b._strData));
+		int Lleft = _strData.size();
+		int Lright = b._strData.size();
+		//相乘的结果不溢出，要求两个数的长度之和要小于机器最大能表示的长度
+		if (Lleft + Lright - 2 < MAXLEN)
+			return BigData(_value * b._value);
+		else
+		{
+			//处理特殊情况  0* ，1*，-1*
+			if (_strData == "+0" || b._strData == "+0")
+			{
+				//cout << 0;
+				return BigData("+0");
+			}
+				
+			if (_strData == "+1" || b._strData == "+1")
+				return (_strData == "+1") ? BigData(b._strData) : BigData(_strData);
+			if (_strData == "-1")
+			{
+				string tmp = b._strData;
+				if (tmp[0] == '-')
+				{
+					tmp[0] = '+';
+					return BigData(tmp);
+				}
+				else
+				{
+					tmp[0] = '-';
+					return BigData(tmp);
+				}
+			}
+			if (b._strData == "-1")
+			{
+				string tmp = _strData;
+				if (tmp[0] == '-')
+				{
+					tmp[0] = '+';
+					return BigData(tmp);
+				}
+				else
+				{
+					tmp[0] = '-';
+					return BigData(tmp);
+				}
+			}
+		}
+			/*return BigData(StrMul(_strData, b._strData));*/
 	}
 
 	friend ostream& operator<<(ostream& _cout, const BigData& b)
@@ -222,6 +329,80 @@ protected:
 			}
 		}
 		return left;
+	}
+
+	string StrMul(string left, string right)
+	{
+		int Lleft = left.size();
+		int Lright = right.size();
+		//长度长的操作数为左操作数
+		if (Lleft < Lright)
+		{
+			left.swap(right);
+			swap(Lleft, Lright);
+		}
+		string strRes;
+		strRes.resize(1);   //这里为什么是1？  -->因为StrAdd时会根据两个操作数的长度重新设定大小(左右操作数长度 长的+1) 
+		/*
+		StrAdd("+" , "263") = "0263"
+		若现在就计算出乘积之后的长度(假设5)，在StrAdd时：
+		StrAdd("+0000","263") = "+00263" strAdd之后每次strRes的size会增加1;
+		所以设置位1，让其随着StrAdd去增加位数；
+		*/
+		int len = strRes.size();
+		strRes[0] = '+';
+
+		/*连个字符串相乘，从低位依次取一个数与另一个数相称,然后把得到的字符串相加；
+		结果存入字符串strRes中
+		9999 * 99                    /strRes
+		1.取第一位  9*99 = 891       /"+891" + "+0" = "+891"
+		2.取次低位  9*99 = 891       /"+8910" + "+891" = "+9810"
+		..
+		最后求得乘积;
+		*/
+
+		int idx = 1;
+		INT64 ret = 1;
+		for (; idx < Lleft; idx++)
+		{
+			INT64 rslt = 0;
+			string tmp = _mul(right, left[Lleft - idx]);//左操作数与右操作数的乘积
+			for (size_t i = idx; i>1; i--)
+			{
+				tmp.append("0");
+			}
+			strRes = StrAdd(tmp, strRes); //两个字符串求和；此处的两个字符串一定是两个正数; / "+8910" + "+891" = "+9810"
+		}
+		if (left[0] != right[0])//两者异号
+		{
+			strRes[0] = '-';
+		}
+		return strRes;
+	}
+
+	string _mul(string left, char right)//右操作数为1位数，左操作数不限(只计算两个正整数的乘积)
+	{
+		if (right == '0')
+			return "+0";
+
+		int Lleft = left.size();
+		int Lright = 1;
+
+		string strRes;
+		strRes.resize(Lleft + 1, '0');
+		strRes[0] = '+';
+		int idx;
+		int step = 0;//进位
+		for (idx = 1; idx < Lleft; idx++)
+		{
+			int num = (left[Lleft - idx] - '0')*(right - '0');
+			step = num / 10;
+			num = num % 10;
+
+			strRes[Lleft - idx + 1] = (num)+(strRes[Lleft - idx + 1] - '0') + '0';
+			strRes[Lleft - idx] = step + '0';
+		}
+		return strRes;
 	}
 private:
 	string _strData;//大数运算的数据
